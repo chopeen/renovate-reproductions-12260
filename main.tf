bfefa77720d20f1437fc5a4dea6d1dd362a33641
Marek Grzenkowicz <chopeen@gmail.com>
2024-09-26 18:52:18 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+module "avm_res_operationalinsights_workspace" {
+  source                                    = "Azure/avm-res-operationalinsights-workspace/azurerm"
+  version                                   = "0.4.0"
+  location                                  = "eu-north"
+  resource_group_name                       = "dummy-rg"
+  name                                      = "dummy-name"
+  enable_telemetry                          = true
+  log_analytics_workspace_retention_in_days = 30
+  log_analytics_workspace_sku               = "PerGB2018"
+  log_analytics_workspace_identity = {
+    type = "SystemAssigned"
+  }
+}
